cde4d6676c7e09c66d5a6a914d78cd286bf66217
Adib <62319714+pihdastudios@users.noreply.github.com>
2025-02-08 19:11:14 +0700

    remove unused directives and update libraries

--- app/src/main/java/com/pihdastudios/blockednumbersbackup/ui/theme/Theme.kt
+++ app/src/main/java/com/pihdastudios/blockednumbersbackup/ui/theme/Theme.kt
@@ -7,7 +7,6 @@
 
 package com.pihdastudios.blockednumbersbackup.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
